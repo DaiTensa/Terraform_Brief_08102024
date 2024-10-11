10e4aa2ee11dca78cbaa2a8140736715d5d1d428
DaiTensa <dai.tensaout@gmail.com>
2024-10-11 16:19:43 +0200

    Deployed ba6c58e with MkDocs version: 1.6.1

--- modules/storage/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Define an output variable named "storage_account_name"
-output "storage_account_name" {
-  value = azurerm_storage_account.asc.name
-  description = "The name of the storage account"
-}
